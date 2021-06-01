919dcc5e764e47231845b9cdf803f07af9a2159c
21grahams <graham.kirsh@gmail.com>
2021-05-31 22:16:34 -0600

    dark mode fully functioning

--- react-context/src/Navbar.js
+++ react-context/src/Navbar.js
@@ -15,9 +15,10 @@ class Navbar extends Component {
   render() {
     console.log('context: ', this.context);
     const { classes } = this.props;
+    const { isDarkMode, toggleTheme } = this.context;
     return (
       <div className={classes.root}>
-        <AppBar position='static' color='primary'>
+        <AppBar position='static' color={isDarkMode ? 'default' : 'primary'}>
           <Toolbar>
             <IconButton className={classes.menuButton} color='inherit'>
               <span>🇫🇷</span>
@@ -25,7 +26,7 @@ class Navbar extends Component {
             <Typography className={classes.title} variant='h6' color='inherit'>
               App Title
             </Typography>
-            <Switch />
+            <Switch onChange={toggleTheme}/>
             <div className={classes.grow} />
             <div className={classes.search}>
               <div className={classes.searchIcon}>
